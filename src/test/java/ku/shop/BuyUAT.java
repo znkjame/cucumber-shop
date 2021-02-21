9c0129ad858d96290cccf44c4e50f846522000ac
ladyusa <usa.sa@ku.th>
2021-02-21 22:30:16 +0700

    update cucumber version

--- src/test/java/ku/shop/BuyUAT.java
+++ src/test/java/ku/shop/BuyUAT.java
@@ -6,8 +6,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "classpath:features",
-                 plugin = {"pretty", "html:target/cucumber"},
-                 strict = true)
+                 plugin = {"pretty", "html:target/cucumber"})
 public class BuyUAT {
 
 }
